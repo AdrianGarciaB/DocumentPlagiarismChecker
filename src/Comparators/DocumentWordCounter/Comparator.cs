056f5ad9f045ca45e71ec22a7be94ed242dc3363
dcalero <dcalero40@gmail.com>
2020-01-16 19:44:56 +0100

    v1

--- src/Comparators/DocumentWordCounter/Comparator.cs
+++ src/Comparators/DocumentWordCounter/Comparator.cs
@@ -35,7 +35,8 @@ namespace DocumentPlagiarismChecker.Comparators.DocumentWordCounter
         /// </summary>
         /// <returns>The matching's results.</returns>
         public override ComparatorMatchingScore Run(){
-            //Counting the words appearences for each document (left and right).
+
+            //Cuenta la pasalbras que aparecen en un documento (iquierda y derecha-).
             Dictionary<string, int[]> counter = new Dictionary<string, int[]>();
             foreach(string word in this.Left.WordAppearances.Select(x => x.Key)){
                 if(!counter.ContainsKey(word)) counter.Add(word, new int[]{0, 0});
@@ -47,7 +48,7 @@ namespace DocumentPlagiarismChecker.Comparators.DocumentWordCounter
                 counter[word][1] += Right.WordAppearances[word];
             }
 
-            //Counting sample file word appearences, in order to ignore those from the previous files.
+            //Comptar les aparences de paraules del fitxer d'exemple, per tal d'ignorar les dels fitxers anteriors.
             if(this.Sample != null){
                  foreach(string word in this.Sample.WordAppearances.Select(x => x.Key)){
                     if(counter.ContainsKey(word)){
